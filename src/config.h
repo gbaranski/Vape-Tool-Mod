4549e6f6c37bfd489f6d0cf9cd469c30bb2c94a3
gbaranski <gbaranski19@gmail.com>
2020-09-08 16:46:32 +0200

    added PWM support

---
+++ src/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#pragma once
+
+#define OUTPUT_PIN 27
+#define OUTPUT_FREQUENCY 200
+#define OUTPUT_RESOLUTION 8
+#define OUTPUT_CHANNEL 1
+#define RESISTANCE 0.22
+
+#define BUTTON_PIN 13
+
+#define BATTERY_PIN 34
+#define ADC_CONV_FACTOR 1.7
+
+#endif
